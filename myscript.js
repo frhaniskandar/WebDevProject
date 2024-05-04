8ef186fda24a6df8412cb2d64267aa9faf48ec64
frhaniskandar <167439026+frhaniskandar@users.noreply.github.com>
2024-05-05 03:31:41 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,25 @@
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active1");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
+
+function validate(){
+
+	alert("Permohonan anda sudah di  terima");
+	}
+
+
